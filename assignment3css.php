d159001617918938ed1b2ce29eddcaa846c6e505
glarson175 <gsl13@pitt.edu>
2016-11-08 15:21:00 -0500

    array
    made an array of colors that change every page reload. trying to be
    clever

--- assignment3css.php
+++ assignment3css.php
@@ -5,10 +5,12 @@
 		$MainColor = "navy";
 		$secondaryColor = "gold";
 		$tertiaryColor = "red";
-		
+		$colors = array("black", "#1A1A1A", "#333333", "#4D4D4D", "#666666", "#808080", "#999999", "#B3B3B3", "#CCCCCC", "#E5E5E5", "white");
+		//for website, want to do cool pictures of pittsbrugh
 ?>
 body{
 	margin-left:20px;
+	background-color:<?php echo $colors[rand(0,10)]?>;
 }
 p{
 	background-color: <?php echo $MainColor; ?>;
